66256098f2e131879e812133778d5432908fd5fd
as <bezotosniyau@yandex.ru>
2023-02-27 20:40:36 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,25 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEWVALUE = "";
+    private int sizeOfEmpty = 10;
+    public String surname;
+    String name;
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int b, int c, int d, int e, int f, int g, int h) {
+
+    }
+
+    Broken() {
+    }
+
+}
