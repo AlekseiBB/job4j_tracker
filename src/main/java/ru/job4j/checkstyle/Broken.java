9a72e8962316f8f248407cd68c8b28dbf8e1e0a4
as <bezotosniyau@yandex.ru>
2023-03-02 09:48:08 +0300

    Правила Checkstyle [#504981 #395786]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,10 +1,13 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
-    public static final String NEWVALUE = "";
     private int sizeOfEmpty = 10;
     public String surname;
+    public static final String NEWVALUE = "";
     String name;
+
+    Broken() {
+    }
 
     void echo() {
     }
@@ -15,11 +18,7 @@ public class Broken {
         }
     }
 
-    void method(int b, int c, int d, int e, int f, int g, int h) {
+    void method(int c, int g, int h) {
 
     }
-
-    Broken() {
-    }
-
 }
