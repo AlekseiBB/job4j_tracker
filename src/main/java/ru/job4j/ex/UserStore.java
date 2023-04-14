b23d9e5cfc5029a21848a1f8fb91b5d5c9b938ea
as <bezotosniyau@yandex.ru>
2023-04-14 09:32:37 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #409318]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -3,38 +3,36 @@ package ru.job4j.ex;
 public class UserStore {
 
     public static User findUser(User[] users, String login) throws UserNotFoundException {
-        int index = 0;
-        boolean result = false;
-        for (int i = 0; i < users.length; i++) {
-            if (users[i].getUsername().equals(login)) {
-                result = true;
-                index = i;
+        User result = null;
+        for (User user : users) {
+            if (user.getUsername().equals(login)) {
+                result = user;
                 break;
             }
         }
-        if (!result) {
-            throw new UserNotFoundException("пользователь не найден");
-        }
-        return users[index];
+            if (result == null) {
+                throw new UserNotFoundException("пользователь не найден");
+            }
+        return result;
     }
 
-    public static boolean validate(User user) throws UserInvalidException {
-        if (user.getUsername() == null || user.getUsername().length() < 3) {
-            throw new UserInvalidException("Пользователь не валидный. Имя должно содержать не менее 3 символов." + user);
+        public static boolean validate (User user) throws UserInvalidException {
+            if (user.getUsername() == null || user.getUsername().length() < 3) {
+                throw new UserInvalidException("Пользователь не валидный. Имя должно содержать не менее 3 символов." + user);
+            }
+            if (!user.isValid()) {
+                throw new UserInvalidException("неверной пользователь " + user);
+            }
+            return false;
         }
-        if (user.isValid() == false) {
-            throw new UserInvalidException("неверной пользователь " + user);
-        }
-        return false;
-    }
 
-    public static void main(String[] args) throws UserNotFoundException {
-        User[] users = {
-                new User("Petr Arsentev", true)
-        };
-        User user = findUser(users, "Petr Arsentev");
-        if (validate(user)) {
-            System.out.println("This user has an access");
+        public static void main (String[]args) throws UserNotFoundException {
+            User[] users = {
+                    new User("Petr Arsentev", true)
+            };
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
         }
     }
-}
