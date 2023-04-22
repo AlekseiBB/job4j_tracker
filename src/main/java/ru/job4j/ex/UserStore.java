de30f8297a886a68a1e76a1588bf75ae4e591229
as <bezotosniyau@yandex.ru>
2023-04-22 10:05:11 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #409318]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -30,9 +30,15 @@ public class UserStore {
             User[] users = {
                     new User("Petr Arsentev", true)
             };
-            User user = findUser(users, "Petr Arsentev");
-            if (validate(user)) {
-                System.out.println("This user has an access");
+            try {
+                User user = findUser(users, "Petr Arsentev");
+                if (validate(user)) {
+                    System.out.println("This user has an access");
+                }
+            } catch (UserInvalidException e) {
+                e.printStackTrace();
+            } catch (UserNotFoundException e) {
+                e.printStackTrace();
             }
         }
     }
