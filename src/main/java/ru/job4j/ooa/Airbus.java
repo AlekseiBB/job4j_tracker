1509d8499614822122dafd99105d58b8f78f5131
as <bezotosniyau@yandex.ru>
2023-05-11 10:49:49 +0300

    3. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,43 @@
+package ru.job4j.ooa;
+
+final class Airbus extends Aircraft {
+    final static int COUNT_ENGINE = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        // использовать дополнительную переменную и тернарный оператор - выгода использования не ясна
+        int engineCount = this.name.equals("A380") ? 4 : COUNT_ENGINE;
+        System.out.println("Количество двигателей: " + engineCount);
+        /*if (this.name.equals("A380")) {
+            System.out.println("Количество двигателей: 4");
+        } else {
+            System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
+        }*/
+
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
