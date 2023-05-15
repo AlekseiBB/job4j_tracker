6e412faee332ca0e2d090ccbf69638b9f014e42e
as <bezotosniyau@yandex.ru>
2023-05-15 20:36:34 +0300

    7. Конвертация двумерного массива в ArrayList [#48580]

--- src/main/java/ru/job4j/CodePatternsToRemember.java
+++ src/main/java/ru/job4j/CodePatternsToRemember.java
@@ -1,37 +1,6 @@
 package ru.job4j;
 
 public class CodePatternsToRemember {
-
-    // объявление инициализация переменных
     int q;
     long b = 99;
-
-    // объявление инициализация массива
-
-
-    // создание метода
-
-    // создание конструктора
-
-    // циклы for , for each, while, do while, swich - case
-
-    //
-
-
-    //
-
-    // поиск в массиве пример метода который будет возвращать index по id
-    /* private int indexOf(int id) {
-        int rsl = -1;
-        for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
-                rsl = index;
-                break;
-            }
-        }
-        return rsl;
-    }
-    */
-
-
 }
