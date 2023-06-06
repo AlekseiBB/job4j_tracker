f8088828e5d103f18b5dadd3488dfc8e096249db
as <bezotosniyau@yandex.ru>
2023-06-06 11:09:14 +0300

    1. Интерфейс Queue [#504942]

---
+++ src/main/java/ru/job4j/queue/Customer.java
@@ -0,0 +1,11 @@
+package ru.job4j.queue;
+
+public class Customer {
+    String name;
+    private int amount;
+
+    public Customer(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+}
