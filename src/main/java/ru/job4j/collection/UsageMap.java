810b664c97c0f3d23bad3147a5c7403286d64392
as <bezotosniyau@yandex.ru>
2023-06-21 11:06:19 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> usageMap = new HashMap<>();
         usageMap.put("alekseiB@yandex.ru", "Aleksei B");
+        usageMap.put("sergeev@yandex.ru", "Sergei");
+        usageMap.put("kyshkow@yandex.ru", "Alex");
         for (String key : usageMap.keySet()) {
             String value = usageMap.get(key);
             System.out.println(key + ": " + value);
