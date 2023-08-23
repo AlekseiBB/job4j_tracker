a16e1224884b522a7817012cc0b52a1a63e71f8e
as <bezotosniyau@yandex.ru>
2023-08-23 09:53:25 +0300

    0. Сортировка [#10096 #438817]

--- src/test/java/ru/job4j/tracker/SortedTest.java
+++ src/test/java/ru/job4j/tracker/SortedTest.java
@@ -1,19 +1,36 @@
 package ru.job4j.tracker;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SortedTest {
 
     @Test
-    public void whenSortAscByName() {
-        /*оставил для примера  [first, second, third, fourth]
-           [first, fourth, second, third]*/
+    public void whenSortItemAscByName() {
+        List<Item> items = Arrays.asList(
+                new Item("first"),
+                new Item("second"),
+                new Item("third"),
+                new Item("fourth")
+        );
+        List<Item> expected = Arrays.asList(
+                new Item("first"),
+                new Item("fourth"),
+                new Item("second"),
+                new Item("third")
+        );
+        Collections.sort(items, new ItemAscByName());
+        assertEquals(expected, items);
+    }
+
+    @Test
+    public void whenSortItemDescByName() {
         List<Item> items = Arrays.asList(
                 new Item("first"),
                 new Item("second"),
@@ -22,12 +39,12 @@ public class SortedTest {
         );
 
         List<Item> expected = Arrays.asList(
-                new Item("first"),
-                new Item("fourth"),
+                new Item("third"),
                 new Item("second"),
-                new Item("third")
+                new Item("fourth"),
+                new Item("first")
         );
-        Collections.sort(items, new ItemAscByName());
+        Collections.sort(items, new ItemDescByName());
         assertEquals(expected, items);
     }
 }
